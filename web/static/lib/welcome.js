96a864aeb9ecaaf0d4a6dc9451484b1fc11b7eba
Zaaksam <zaaksam@qq.com>
2018-02-08 01:01:44 +0800

    去除webview对象判断，存在兼容问题

--- web/static/lib/welcome.js
+++ web/static/lib/welcome.js
@@ -8,11 +8,6 @@ function WelcomeViewModel() {
     self.title = C.appName + ' ' + C.appVersion
 
     self.onLoad = function () {
-        if (!(window && window.external && window.external.invoke)) {
-            self.isConnect(true);
-            self.btnText('go层交互对象错误');
-            return;
-        }
         self.isLoad = true;
 
         self.onOpen();
@@ -49,7 +44,7 @@ function WelcomeViewModel() {
 
         if (data === 'connect') {
             self.isConnect(true);
-            self.btnText('goman已在浏览器中打开');
+            self.btnText('goman已在浏览器打开');
         } else if (data === 'disconnect') {
             self.isConnect(false);
             self.btnText('在浏览器中打开goman');
