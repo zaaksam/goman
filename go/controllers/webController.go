9403f7dd62bd8fd7ae5472c0c1bf671a85910473
Zaaksam <zaaksam@qq.com>
2018-02-07 20:27:44 +0800

    增加web版欢迎页面

--- go/controllers/webController.go
+++ go/controllers/webController.go
@@ -17,11 +17,44 @@ type WebController struct {
 	beego.Controller
 }
 
-// Get 主页
-func (c *WebController) Get() {
+// Welcome 浏览器版本欢迎页
+func (c *WebController) Welcome() {
+	windowsDebug := ""
+
 	var unix int64
 	if config.AppConf.Debug {
 		unix = time.Now().Unix()
+
+		if config.AppConf.OS == config.OS_TYPE_WINDOWS {
+			windowsDebug = `<script type="text/javascript" src="https://getfirebug.com/firebug-lite.js"></script>`
+		}
+	} else {
+		unix = config.AppConf.Started
+	}
+	unixStr := "?t=" + strconv.FormatInt(unix, 10)
+
+	body := strings.Replace(views.Welcome, "{{.unix}}", unixStr, -1)
+	body = strings.Replace(body, "{{.appName}}", config.AppConf.Name, -1)
+	body = strings.Replace(body, "{{.windowsDebug}}", windowsDebug, -1)
+
+	if c.Ctx.ResponseWriter.Header().Get("Content-Type") == "" {
+		c.Ctx.Output.Header("Content-Type", "text/html; charset=utf-8")
+	}
+
+	c.Ctx.Output.Body([]byte(body))
+}
+
+// Get 主页
+func (c *WebController) Get() {
+	windowsDebug := ""
+
+	var unix int64
+	if config.AppConf.Debug {
+		unix = time.Now().Unix()
+
+		if config.AppConf.OS == config.OS_TYPE_WINDOWS {
+			windowsDebug = `<script type="text/javascript" src="https://getfirebug.com/firebug-lite.js"></script>`
+		}
 	} else {
 		unix = config.AppConf.Started
 	}
@@ -29,6 +62,7 @@ func (c *WebController) Get() {
 
 	body := strings.Replace(views.Index, "{{.unix}}", unixStr, -1)
 	body = strings.Replace(body, "{{.appName}}", config.AppConf.Name, -1)
+	body = strings.Replace(body, "{{.windowsDebug}}", windowsDebug, -1)
 
 	if c.Ctx.ResponseWriter.Header().Get("Content-Type") == "" {
 		c.Ctx.Output.Header("Content-Type", "text/html; charset=utf-8")
@@ -47,11 +81,16 @@ func (c *WebController) Config() {
 	conf := &struct {
 		Name      string `json:"appName"`
 		Version   string `json:"appVersion"`
+		RunMode   string `json:"runMode"`
 		UserAgent string `json:"userAgent"`
 	}{
 		Name:      config.AppConf.Name,
 		Version:   config.AppConf.Version,
 		UserAgent: c.Ctx.Input.Header("User-Agent"),
+	}
+
+	if config.AppConf.RunMode == config.RUN_MODE_WEB {
+		conf.RunMode = "web"
 	}
 
 	j, _ := json.Marshal(conf)
