332efe3e5724976a3b2d3b800ecb127d607bf521
Zaaksam <zaaksam@qq.com>
2018-02-08 00:17:20 +0800

    增强webview的使用场景，支持js交互

--- go/goman_webview.go
+++ go/goman_webview.go
@@ -5,76 +5,101 @@ package goman
 import (
 	"fmt"
 	"os/exec"
-	"runtime"
 	"time"
 
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/logs"
 	"github.com/zaaksam/goman/go/config"
+	"github.com/zaaksam/goman/go/services"
 	"github.com/zserge/webview"
 )
 
 func run(mode string) {
 	if mode == "web" {
-		config.AppConf.IsAppMode = false
-		config.AppConf.IsWebMode = true
+		config.AppConf.RunMode = config.RUN_MODE_WEB
 	} else if mode == "app" {
-		config.AppConf.IsAppMode = true
-		config.AppConf.IsWebMode = false
+		config.AppConf.RunMode = config.RUN_MODE_APP
 	}
 
-	if config.AppConf.IsWebMode {
+	url := fmt.Sprintf("%s/web", config.AppConf.Host)
+
+	if config.AppConf.OS == config.OS_TYPE_LINUX {
 		logs.Info("====== 欢迎使用 " + config.AppConf.Name + " " + config.AppConf.Version + "（Web模式） ，关闭此窗口即可退出程序 ======")
 
 		if !config.AppConf.Debug {
-			go openBrowser()
+			time.Sleep(1 * time.Second)
+			go openBrowser(url)
 		}
 
 		beego.Run()
-	} else if config.AppConf.IsAppMode {
-		//logs.Info("====== 欢迎使用 " + config.AppConf.Name + " " + config.AppConf.Version + "（App模式） ，关闭此窗口即可退出程序 ======")
 
-		go beego.Run()
-
-		// w := webview.New(webview.Settings{
-		// 	Title:     config.AppConf.Name,
-		// 	URL:       fmt.Sprintf("http://%s:%d/web", config.AppConf.IP, config.AppConf.Port),
-		// 	Width:     800,
-		// 	Height:    600,
-		// 	Resizable: true,
-		// 	ExternalInvokeCallback: func(w webview.WebView, data string) {
-		// 		logs.Info("收到JS信息：", data)
-		// 	},
-		// })
-		// defer w.Exit()
-
-		// w.Run()
-
-		err := webview.Open(config.AppConf.Name, fmt.Sprintf("http://%s:%d/web", config.AppConf.IP, config.AppConf.Port), 1024, 768, true)
-		if err != nil {
-			logs.Critical("webview启动失败：", err)
-		}
-	}
-}
-
-func openBrowser() {
-	time.Sleep(2 * time.Second)
-
-	cmds := map[string][]string{
-		"windows": []string{"cmd", "/c", "start", ""},
-		"darwin":  []string{"open", ""},
-		"linux":   []string{"xdg-open", ""},
-	}
-
-	osStr := runtime.GOOS
-
-	args, ok := cmds[osStr]
-	if !ok {
-		logs.Error("非预设系统，无法打开浏览器：", osStr)
 		return
 	}
 
-	args[len(args)-1] = fmt.Sprintf("http://%s:%d/web", config.AppConf.IP, config.AppConf.Port)
+	go beego.Run()
+
+	settings := webview.Settings{
+		Title:     config.AppConf.Name,
+		URL:       url,
+		Width:     1280,
+		Height:    900,
+		Resizable: true,
+		Debug:     config.AppConf.Debug,
+		ExternalInvokeCallback: func(w webview.WebView, data string) {
+			switch data {
+			case "check":
+				isConnect := services.App.IsConnect()
+				if isConnect {
+					callJs(w, "connect")
+				} else {
+					callJs(w, "disconnect")
+				}
+			case "open":
+				openBrowser(url)
+			case "github":
+				openBrowser("https://github.com/zaaksam/goman")
+			case "gitee":
+				openBrowser("https://gitee.com/zaaksam/goman")
+			case "close":
+				w.Terminate()
+				w.Exit()
+			}
+
+		},
+	}
+
+	if config.AppConf.RunMode == config.RUN_MODE_WEB {
+		settings.URL += "/welcome"
+		settings.Width = 500
+		settings.Height = 300
+		settings.Resizable = false
+	}
+
+	w := webview.New(settings)
+	w.Run()
+}
+
+func callJs(w webview.WebView, data string) {
+	err := w.Eval("vm.callJs('" + data + "');")
+	if err != nil {
+		logs.Error("向js发送消息失败：", err)
+	}
+}
+
+func openBrowser(url string) {
+	cmds := map[config.OSType][]string{
+		config.OS_TYPE_WINDOWS: []string{"cmd", "/c", "start", ""},
+		config.OS_TYPE_DARWIN:  []string{"open", ""},
+		config.OS_TYPE_LINUX:   []string{"xdg-open", ""},
+	}
+
+	args, ok := cmds[config.AppConf.OS]
+	if !ok {
+		logs.Error("无法打开浏览器：", config.AppConf.OS)
+		return
+	}
+
+	args[len(args)-1] = url
 
 	c := exec.Command(args[0], args[1:]...)
 	err := c.Start()
