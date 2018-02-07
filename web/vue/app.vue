f3496dfe94d83e78776cfcb5ffe5663326eec4ee
Zaaksam <zaaksam@qq.com>
2018-02-07 20:30:58 +0800

    web版增加心跳包发送

--- web/vue/app.vue
+++ web/vue/app.vue
@@ -17,34 +17,49 @@
 
 <style>
 .layout {
-  border: 1px solid #d7dde4;
-  background: #f5f7f9;
-  position: relative;
-  border-radius: 4px;
-  overflow: hidden;
+    border: 1px solid #d7dde4;
+    background: #f5f7f9;
+    position: relative;
+    border-radius: 4px;
+    overflow: hidden;
 }
 
 .layout-content {
-  min-height: 200px;
-  margin: 15px;
-  overflow: hidden;
-  background: #fff;
-  border-radius: 4px;
+    min-height: 200px;
+    margin: 15px;
+    overflow: hidden;
+    background: #fff;
+    border-radius: 4px;
 }
 
 .layout-content-main {
-  padding: 10px;
+    padding: 10px;
 }
 </style>
 
 <script lang="ts">
-import { Vue, Component } from "vue-property-decorator";
-import MyTop from "./top.vue";
+import { Vue, Component } from 'vue-property-decorator'
+import MyTop from './top.vue'
+import API from '../ts/api'
 
 @Component({
-  components: {
-    MyTop
-  }
+    components: {
+        MyTop
+    }
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+    mounted() {
+        if (C.runMode === 'web') {
+            this.heartbeat()
+        }
+    }
+
+    heartbeat() {
+        //发送心跳包
+        setTimeout(async () => {
+            await API.get<any>('/app')
+            this.heartbeat()
+        }, 1000)
+    }
+}
 </script>
